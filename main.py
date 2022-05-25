bf25dc0adad7aebf3ce4ab3b1b7243666cd78700
Trent Townsend <trent.townsend2@gmail.com>
2022-05-25 15:43:36 +1000

    Change times from local to US/EST to match time of stockmarket

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
-from urllib import request
-import requests 
-from datetime import date, datetime, timedelta 
+
+import requests
+from datetime import date, datetime, timedelta
+import pytz
 import os
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
-yesterday = str(date.today() - timedelta(days=1))
-two_days_ago = str(date.today() - timedelta(days=2))
+# Determine date in US/EST time currently
+us_est = pytz.timezone("US/Eastern")
+now_est = (datetime.now(us_est)).date() - timedelta(days=1)
+yesterday_us_est = now_est - timedelta(days=1)
 
-#Get request for stock data 
+# #Get request for stock data
 ALPHA_API_KEY = os.environ['ALPHA_API_KEY']
-
 
 api_parameters = {
     "function": "TIME_SERIES_DAILY",
-    "symbol" : "TSLA",
-    "outputsize" : "compact",
-    "apikey" : ALPHA_API_KEY
+    "symbol": "TSLA",
+    "outputsize": "compact",
+    "apikey": ALPHA_API_KEY
 }
 
-alpha_requests = requests.get(url="https://www.alphavantage.co/query", params=api_parameters)
+alpha_requests = requests.get(
+    url="https://www.alphavantage.co/query", params=api_parameters)
 alpha_requests.raise_for_status()
 data = alpha_requests.json()
 
-#Extract two days most recent data
-data_yesterday = data["Time Series (Daily)"][yesterday]
-data_two_days_ago = data["Time Series (Daily)"][two_days_ago]
+
+# Extract two days most recent data
+
+data_today = data["Time Series (Daily)"][str(now_est)]
+data_yesterday = data["Time Series (Daily)"][str(yesterday_us_est)]
+
+print(data_today)
+print(data_yesterday)
